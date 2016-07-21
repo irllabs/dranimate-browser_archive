c23f764609ac571f9aafcbd91f0a58535515bba3
Paul Peng <paul1peng@gmail.com>
2016-07-21 00:30:56 -0400

    Mock up puppet dashboard (except for header)
    Small changes to try to make: get the sliders to always be filled in; maybe the two warning buttons need to be different shades. Big changes: look into filters to make sure inputs are always filled and "look right" (i.e., always in range and appended with unit (px, %, degree, etc.).

--- src/ui/puppet-dashboard/puppet-dashboard.js
+++ src/ui/puppet-dashboard/puppet-dashboard.js
@@ -1,22 +1,23 @@
 /** Dranimate Browser UI - Puppet Dashboard
  *
- * Module Name: 'app.puppetDashboard'
- * Directive Name: <puppet-dashboard>
+ * Module Name: 'dran.puppetDashboard'
+ * Directive Name: <dran-puppet-dashboard>
  */
 
-angular.module('app.puppetDashboard', ['ngMaterial'])
-  .directive('puppetDashboard', function() {
+angular.module('dran.puppetDashboard', ['ngMaterial'])
+  .directive('dranPuppetDashboard', function() {
     return {
       restrict: 'AE',
-      templateUrl: 'puppet-dashboard.html',
+      templateUrl: 'src/ui/puppet-dashboard/puppet-dashboard.html',
       scope: { },
       controller: ['$scope', function($scope) {
         // TODO: connect to current puppet (dummy values for now)
         $scope.pupdash = {
           x: 60,
           y: 75,
-          rotation: 54,
-          scale: 100
+          rotation: 0,
+          scaleX: 100,
+          scaleY: 100
         }
       }]
     };
