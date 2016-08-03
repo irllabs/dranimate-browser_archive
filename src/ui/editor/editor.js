4fa6b9e38447aea93ec23af9f8d6d59ca641757c
Paul Peng <paul1peng@gmail.com>
2016-08-03 15:25:47 -0400

    Clean up editor.js code

--- src/ui/editor/editor.js
+++ src/ui/editor/editor.js
@@ -1,9 +1,16 @@
 /** Dranimate Browser UI - Editor
  *
  * Module Name: 'dran.editor'
- * Directive Name: <dran-editor>
  *
- * It's the editor!
+ * Directive: dran-three-container (Element, Attribute)
+ *   Container for the main Dranimate canvas to go into.
+ *   TODO: Rename it to something that doesn't couple it to three.js?
+ *
+ * Directive: dran-toggle-pupdash (Attribute)
+ *   Clicking on it will toggle the puppet dashboard open and closed!
+ *
+ * Component: dran-editor (Element)
+ *   The editor!
  *
  * Note: currently (is supposed to) contain input for editing puppet name
  * in the left sidenav (pupdash-container). Move this to the sidenav?
@@ -11,38 +18,36 @@
  *   But maybe we could get around that using directive attributes.
  */
 
-angular.module('dran.editor', [
-    'ngMaterial',
-    'dran.editor.puppetDashboard',
-    'dran.editor.zoompanner',
-    'dran.model'
-  ])
+(function() {
 
-  .directive('dranThreeContainer', ['model', function(model) {
-    return {
-      restrict: 'AE',
-      template: '<div></div>',
-      scope: { },
-      link: function(scope, element) {
-        /* element[0] gets the raw DOM reference from the jqlite object */
-        model.setup(element[0]);
-      }
-    };
-  }])
+var edMod = angular.module('dran.editor', [
+  'ngMaterial',
+  'dran.editor.puppetDashboard',
+  'dran.editor.zoompanner',
+  'dran.model'
+]);
 
-  .directive('dranEditor', function() {
-    return {
-      restrict: 'AE',
-      templateUrl: 'src/ui/editor/editor.html',
-      scope: { },
-      controller: ['$scope', '$mdSidenav', '$mdMedia', function($scope, $mdSidenav, $mdMedia) {
-        $scope.togglePupdashContainer = function() {
-          $mdSidenav('pupdash-container').toggle();
-        };
+edMod.directive('dranThreeContainer', ['model', function(model) {
+  return {
+    restrict: 'AE',
+    link: function(scope, element) {
+      /* element[0] gets the raw DOM reference from the jqlite object */
+      model.setup(element[0]);
+    }
+  };
+}]);
 
-        $scope.getPupdashContainerDepth = function() {
-          return $mdMedia('gt-sm') ? 2 : 4;
-        };
-      }]
-    };
-  });
+edMod.directive('dranTogglePupdash', ['$mdSidenav', function($mdSidenav) {
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      element.bind('click', function() {
+        $mdSidenav('pupdash-container').toggle();
+      });
+    }
+  };
+}]);
+
+edMod.component('dranEditor', { templateUrl: 'src/ui/editor/editor.html' });
+
+})();
