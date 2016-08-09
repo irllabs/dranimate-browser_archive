40d4fd2e891850dbe78764102617883ab52961b4
Paul Peng <paul1peng@gmail.com>
2016-08-09 15:43:28 -0400

    Safely(?) digest on changes to dranimate model and imageToMesh object

--- src/ui/model/image-to-mesh.js
+++ src/ui/model/image-to-mesh.js
@@ -7,10 +7,17 @@ window.imageToMesh = new ImageToMesh(); // for debug. comment out for prod!
 
 var imgToMeshMod = angular.module('dran.image-to-mesh', [ ]);
 
-imgToMeshMod.factory('imageToMesh', function() {
+imgToMeshMod.factory('imageToMesh', ['$rootScope', function($rootScope) {
   var imageToMesh = window.imageToMesh; // for debug. comment out for prod!
   // var imageToMesh = new imageToMesh(); // uncomment for production!
+  imageToMesh.onChange(function() {
+    /* $evalAsync forces a digest (i.e. angular update) cycle
+     * when called while one isn't already happening.
+     * Allows view to update from the model.
+     */
+    $rootScope.$evalAsync(function() { });
+  });
   return imageToMesh;
-});
+}]);
 
 })();
--- src/ui/model/model.js
+++ src/ui/model/model.js
@@ -8,10 +8,17 @@ window.dranimate = new Dranimate(); // for debug. comment out for production!
 
 var modelMod = angular.module('dran.model', [ ]);
 
-modelMod.factory('model', function() {
+modelMod.factory('model', ['$rootScope', function($rootScope) {
   var dranimate = window.dranimate; // for debug. comment out for production!
   // var dranimate = new Dranimate(); // uncomment for production!
+  dranimate.onChange(function() {
+    /* $evalAsync forces a digest (i.e. angular update) cycle
+     * when called while one isn't already happening.
+     * Allows view to update from the model.
+     */
+    $rootScope.$evalAsync(function() { });
+  });
   return dranimate;
-});
+}]);
 
 })();
